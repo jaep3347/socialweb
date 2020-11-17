8ff24ae81cbf94a9773aca63f94f871932624235
Grace Kim <haeunk@andrew.cmu.edu>
2020-11-17 00:25:00 -0500

    clean up

--- server/server.js
+++ server/server.js
@@ -10,8 +10,7 @@ app.use(morgan("dev"));
 app.get("/api/v1/posts", async (req, res) => {
     try {
       const posts = await db.query(
-          // our own sql query here
-        "select * from restaurants left join (select restaurant_id, COUNT(*), TRUNC(AVG(rating),1) as average_rating from reviews group by restaurant_id) reviews on restaurants.id = reviews.restaurant_id;"
+        "select * from posts order by time_posted asc;"
       );
   
       res.status(200).json({
@@ -27,28 +26,23 @@ app.get("/api/v1/posts", async (req, res) => {
 });
 
 // Get Feed from Search
-app.get("/api/v1/posts/:id", async (req, res) => {
-    console.log(req.params.id);
-  
+app.get("/api/v1/posts/:name", async (req, res) => {
+    console.log(req.params.name);
+
     try {
-      const restaurant = await db.query(
-          // our own sql query here
-        "select * from restaurants left join (select restaurant_id, COUNT(*), TRUNC(AVG(rating),1) as average_rating from reviews group by restaurant_id) reviews on restaurants.id = reviews.restaurant_id where id = $1",
-        [req.params.id]
+      const posts = await db.query(
+        'select * from posts where author = ' + req.params.name.replace(/"/g, '\'')
       );
-      // select * from restaurants wehre id = req.params.id
-  
-      const reviews = await db.query(
-        "select * from reviews where restaurant_id = $1",
-        [req.params.id]
+    
+      const comments = await db.query(
+        'select * from comments where author = ' + req.params.name.replace(/"/g, '\'')
       );
-      console.log(reviews);
   
       res.status(200).json({
         status: "succes",
         data: {
-          restaurant: restaurant.rows[0],
-          reviews: reviews.rows,
+          posts: posts.rows,
+          comments: comments.rows,
         },
       });
     } catch (err) {
@@ -59,18 +53,17 @@ app.get("/api/v1/posts/:id", async (req, res) => {
 // Create Post
 app.post("/api/v1/posts", async (req, res) => {
     console.log(req.body);
-  
+
     try {
       const results = await db.query(
-          // our own sql query here
-        "INSERT INTO restaurants (name, location, price_range) values ($1, $2, $3) returning *",
-        [req.body.name, req.body.location, req.body.price_range]
+        "insert into posts (author, content, time_posted) values ($1, $2, now()) returning *;",
+        [test.author, test.content]
       );
       console.log(results);
       res.status(201).json({
         status: "succes",
         data: {
-          restaurant: results.rows[0],
+          post: results.rows[0],
         },
       });
     } catch (err) {
@@ -78,18 +71,18 @@ app.post("/api/v1/posts", async (req, res) => {
     }
   });
 
-  app.post("/api/v1/posts/:id/addComment", async (req, res) => {
+// Add Comment to Post
+app.post("/api/v1/posts/:id/addComment", async (req, res) => {
     try {
       const newComment = await db.query(
-          // our own sql query here
-        "INSERT INTO reviews (restaurant_id, name, review, rating) values ($1, $2, $3, $4) returning *;",
-        [req.params.id, req.body.name, req.body.review, req.body.rating]
+        "insert into comments (pid, author, content, time_posted) values ($1, $2, $3, now()) returning *;",
+        [req.params.id, req.body.author, req.body.content]
       );
       console.log(newComment);
       res.status(201).json({
         status: "success",
         data: {
-          review: newComment.rows[0],
+          comment: newComment.rows[0],
         },
       });
     } catch (err) {
