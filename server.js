1727be0d8c2487f5d22124a5e2c5722add17908d
Grace Kim <haeunk@andrew.cmu.edu>
2020-12-14 18:59:22 -0800

    favorites

--- server.js
+++ server.js
@@ -4,78 +4,56 @@ const db = require("./db");
 const app = express();
 var path = require('path');
 var bodyParser = require('body-parser');
-const bcrypt = require('bcrypt');
-const { request } = require("http");
-const { response } = require("express");
-const passport = require('passport');
-const flash = require('express-flash')
 const session = require('express-session')
-const methodOverride = require('method-override')
-
-
-const initializePassport = require('./passport-config');
-initializePassport(
-  passport,
-  email => users.find(user => user.email === email),
-  id => users.find(user => user.id === id)
-)
-const users = [];
-let numComments = 0;
-let numPosts = 0;
-
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(express.urlencoded({extended: false}));
-app.use(flash())
 app.use(session({
-  secret: process.env.SESSION_SECRET,
-  resave: false,
-  saveUninitialized: false
-}))
-app.use(passport.initialize())
-app.use(passport.session())
-app.use(methodOverride('_method'))
-
+	secret: 'secret',
+	resave: true,
+	saveUninitialized: true
+}));
 
 // Get all posts (Home Page)
-app.get("/", checkAuthenticated, async (req, res) => {
-    try {
-      const posts = await db.query(
-        "select * from posts order by time_posted desc;"
-      );
-
-      res.render("pages/index", {
-        feed: "Recent Posts",
-        posts: posts.rows,
-        name: req.user.name,
-        numComments: numComments,
-        numPosts: numPosts
-      });
-
-    } catch (err) {
-      console.log(err);
-    }
-});
-
-// Guest Page
-app.get("/guest", checkNotAuthenticated, async (req, res) => {
+app.get("/", async (req, res) => {
+  
   try {
     const posts = await db.query(
       "select * from posts order by time_posted desc;"
     );
 
-    res.render("pages/guest", {
-      feed: "Recent Posts",
-      posts: posts.rows
-    });
+    const favorites = await db.query(
+      "select p.content from posts as p join favorites as f on p.pid = f.pid where f.uid = $1;",
+      [req.session.uid]
+    );
+
+    console.log(favorites.rows);
+
+    if (req.session.loggedin) {
+      res.render("pages/index", {
+        loggedin: true,  
+        feed: "Recent Posts",
+        posts: posts.rows,
+        name: req.session.name,
+        favorites: favorites.rows,
+        numComments: req.session.numComments,
+        numPosts: req.session.numPosts
+      });
+    } else {
+      res.render("pages/index", {
+        loggedin: false,  
+        feed: "Recent Posts",
+        posts: posts.rows,
+      });
+    }
   } catch (err) {
     console.log(err);
   }
-})
+});
+
 
 // Log In Page
-app.get("/login", checkNotAuthenticated, async (req, res) => {
+app.get("/login", async (req, res) => {
   try {
     res.render("pages/login")
   } catch (err) {
@@ -84,15 +62,31 @@ app.get("/login", checkNotAuthenticated, async (req, res) => {
 })
 
 // Log In Page POST
-app.post('/login', checkNotAuthenticated, passport.authenticate('local', {
-  successRedirect: '/',
-  failureRedirect: '/login',
-  failureFlash: true
-}))
+app.post('/login', async (req, res) => {
+	if (req.body.password && req.body.password) {
+    const result = await db.query(
+      "select * from users where username = $1 AND password = $2", 
+      [req.body.username, req.body.password]
+    );
 
+    if (result.rows[0]) {
+      req.session.loggedin = true;
+      req.session.name = result.rows[0].name;
+      req.session.uid = result.rows[0].uid;
+      req.session.numPosts = result.rows[0].posts;
+      req.session.numComments = result.rows[0].comments;
+      res.redirect('/');
+    } else {
+      res.send('Incorrect Username and/or Password!');
+    }
+	} else {
+		res.send('Please enter Username and Password!');
+		res.end();
+	}
+});
 
 // Sign Up Page
-app.get("/register", checkNotAuthenticated, async (req, res) => {
+app.get("/register", async (req, res) => {
   try {
     res.render("pages/register")
   } catch (err) {
@@ -101,22 +95,19 @@ app.get("/register", checkNotAuthenticated, async (req, res) => {
 })
 
 // Sign Up Page POST
-app.post("/register", checkNotAuthenticated, async (req, res) => {
+app.post("/register", async (req, res) => {
   try {
-    const hashedPassword = await bcrypt.hash(req.body.password, 10)
-    users.push({
-      id: Date.now().toString(),
-      name: req.body.name,
-      email: req.body.email,
-      password: hashedPassword
-    })
+    await db.query(
+      "insert into users (name, username, password, comments, posts) values ($1, $2, $3, 0, 0);",
+      [req.body.name, req.body.username, req.body.password]
+    );
+    console.log("done");
     res.redirect('/login')
   } catch (err) {
-    res.redirect('/register')
     console.log(err);
+    res.redirect('/register');
   }
-  console.log(users);
-})
+});
 
 // Get feed from search
 app.post("/posts", async (req, res) => {
@@ -128,26 +119,24 @@ app.post("/posts", async (req, res) => {
     console.log(keywords);
 
     try {
-
       const posts = await db.query(
         "select * from posts where content ilike '%" + keywords.join("%' or content ilike '%") + "%';"
       )
 
-      if (req.isAuthenticated()) {
+      if (req.session.loggedin) {
         res.render("pages/index", {
           feed: "Posts for " + req.body.terms,
           posts: posts.rows,
-          name: req.user.name,
-          numComments: numComments,
-          numPosts: numPosts
+          name: req.session.name,
+          numComments: req.session.numComments,
+          numPosts: req.session.numPosts
         });
       } else {
-        res.render("pages/guest", {
+        res.render("pages/index", {
           feed: "Posts for " + req.body.terms,
-          posts: posts.rows
-        })
+          posts: posts.rows,
+        });
       }
-
     } catch (err) {
       console.log(err);
     }
@@ -155,30 +144,45 @@ app.post("/posts", async (req, res) => {
 
 // Get post
 app.get("/posts/:id", async (req, res) => {    
-    try {
-      const post = await db.query(
-        "select * from posts where pid = " + req.params.id
-      );
+  try {
+    const post = await db.query(
+      "select p.content, u.name from posts as p join users as u on p.uid = u.uid where p.pid = $1;",
+      [req.params.id]
+    );
 
-      const comments = await db.query(
-        "select * from comments where pid = " + req.params.id
-      );
+    const comments = await db.query(
+      "select c.content, u.name from comments as c join users as u on c.uid = u.uid where c.pid = $1;",
+      [req.params.id]
+    );
 
-      if (req.isAuthenticated()) {
-        username = req.user.name
-      } else {
-        username = null
-      }
+    var favorite = await db.query(
+      "select * from favorites where pid = $1 and uid = $2;",
+      [req.params.id, req.session.uid]
+    )
 
+    if (favorite.rows[0]) {
+      favorite = true;
+    } 
+
+    if (req.session.loggedin) {
       res.render("pages/post", {
         post: post.rows[0],
         comments: comments.rows,
-        name: username
+        loggedin: true,
+        favorite: favorite
       });
-
-    } catch (err) {
-      console.log(err);
+    } else {
+      res.render("pages/post", {
+        post: post.rows[0],
+        comments: comments.rows,
+        loggedin: false,
+        favorite: false
+      });
     }
+
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 // New post form
@@ -188,65 +192,64 @@ app.get("/new_post.html", (req, res) => {
 
 // Create post
 app.post("/posts/new", async (req, res) => {
-    console.log(req.body);
+  console.log(req.body);
 
-    try {
-      const results = await db.query(
-        "insert into posts (author, content, time_posted) values ($1, $2, now()) returning *;",
-        [req.body.author, req.body.content]
-      );
-      numPosts += 1;
-      console.log(numPosts);
-      console.log(results);
+  try {
+    const results = await db.query(
+      "insert into posts (uid, content, time_posted) values ($1, $2, now()) returning *;",
+      [req.session.uid, req.body.content]
+    );
 
-      res.redirect("/posts/" + results.rows[0].pid);
-    } catch (err) {
-      console.log(err);
-    }
+    req.session.numPosts += 1;
+
+    res.redirect("/posts/" + results.rows[0].pid);
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+// Favorite post
+app.post("/posts/:id/addFavorite", async (req, res) => {
+  try {
+    await db.query(
+      "insert into favorites (pid, uid) values ($1, $2);",
+      [req.params.id, req.session.uid]
+    );
+    res.redirect("/posts/" + req.params.id);
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 // Add comment to post
 app.post("/posts/:id/addComment", async (req, res) => {
-    console.log(req.params.id)
+  console.log(req.params.id)
 
-    try {
-      const new_comment = await db.query(
-        "insert into comments (pid, author, content, time_posted) values ($1, $2, $3, now()) returning *;",
-        [req.params.id, req.body.author, req.body.content]
-      );
-      numComments += 1;
-      console.log(numComments);
-      console.log(new_comment);
+  try {
+    await db.query(
+      "insert into comments (pid, uid, content, time_posted) values ($1, $2, $3, now()) returning *;",
+      [req.params.id, req.session.uid, req.body.content]
+    );
 
-      res.redirect("/posts/" + req.params.id);
-    } catch (err) {
-      console.log(err);
-    }
-  });
+    req.session.numComments += 1;
+    await db.query(
+      "update users set comments = $1 where uid = $2;",
+      [req.session.numComments + 1, req.session.uid]
+    );
 
+    res.redirect("/posts/" + req.params.id);
+  } catch (err) {
+    console.log(err);
+  }
+});
 
-function checkAuthenticated(req, res, next) {
-    if (req.isAuthenticated()) {
-      return next()
-    }
-  
-    res.redirect('/guest')
-}
-  
-function checkNotAuthenticated(req, res, next) {
-    if (req.isAuthenticated()) {
-      return res.redirect('/')
-    }
-    next()
-}
-
-app.delete('/logout', (req, res) => {
-  req.logOut()
-  res.redirect('/')
-})
+app.post("/logout", (req, res) => {
+  req.session.destroy();
+  res.redirect("/");
+});
 
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
-    console.log(`server is up and listening on port ${port}`);
+  console.log(`server is up and listening on port ${port}`);
 });
 
