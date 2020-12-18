fa4a91a6a9783d1ed8f9acda631373a8188c1c09
Grace Kim <haeunk@andrew.cmu.edu>
2020-12-18 04:16:40 -0800

    updated sql file

--- db/db.sql
+++ db/db.sql
@@ -1,39 +1,38 @@
--- Table: public.posts
+-- Table: public.users
 
--- DROP TABLE public.posts;
+-- DROP TABLE public.users;
 
-CREATE TABLE public.posts
+CREATE TABLE public.users
 (
-    author character varying(255) COLLATE pg_catalog."default" NOT NULL,
-    pid integer NOT NULL DEFAULT nextval('"Posts_pid_seq"'::regclass),
-    content text COLLATE pg_catalog."default" NOT NULL,
-    time_posted timestamp with time zone NOT NULL,
-    CONSTRAINT "Posts_pkey" PRIMARY KEY (pid)
+    uid integer NOT NULL DEFAULT nextval('credentials_uid_seq'::regclass),
+    username character varying COLLATE pg_catalog."default" NOT NULL,
+    password character varying COLLATE pg_catalog."default" NOT NULL,
+    name character varying COLLATE pg_catalog."default" NOT NULL,
+    posts integer NOT NULL,
+    comments integer NOT NULL,
+    CONSTRAINT users_pkey PRIMARY KEY (uid)
 )
 WITH (
     OIDS = FALSE
 )
 TABLESPACE pg_default;
 
-ALTER TABLE public.posts
+ALTER TABLE public.users
     OWNER to postgres;
 
+-- Table: public.posts
 
+-- DROP TABLE public.posts;
 
--- Table: public.comments
-
--- DROP TABLE public.comments;
-
-CREATE TABLE public.comments
+CREATE TABLE public.posts
 (
-    cid integer NOT NULL DEFAULT nextval('"Comments_cid_seq"'::regclass),
     content text COLLATE pg_catalog."default" NOT NULL,
-    author character varying(255) COLLATE pg_catalog."default" NOT NULL,
     time_posted timestamp with time zone NOT NULL,
-    pid integer NOT NULL,
-    CONSTRAINT "Comments_pkey" PRIMARY KEY (cid),
-    CONSTRAINT "Posts_fkey" FOREIGN KEY (pid)
-        REFERENCES public.posts (pid) MATCH SIMPLE
+    pid integer NOT NULL DEFAULT nextval('posts_pid_seq'::regclass),
+    uid integer NOT NULL,
+    CONSTRAINT posts_pkey PRIMARY KEY (pid),
+    CONSTRAINT posts_fkey FOREIGN KEY (uid)
+        REFERENCES public.users (uid) MATCH SIMPLE
         ON UPDATE NO ACTION
         ON DELETE NO ACTION
         NOT VALID
@@ -43,5 +42,81 @@ WITH (
 )
 TABLESPACE pg_default;
 
+ALTER TABLE public.posts
+    OWNER to postgres;
+
+-- Table: public.comments
+
+-- DROP TABLE public.comments;
+
+CREATE TABLE public.comments
+(
+    content text COLLATE pg_catalog."default" NOT NULL,
+    time_posted timestamp with time zone NOT NULL,
+    cid integer NOT NULL DEFAULT nextval('comments_cid_seq'::regclass),
+    pid integer NOT NULL,
+    uid integer NOT NULL,
+    CONSTRAINT comments_cid PRIMARY KEY (cid),
+    CONSTRAINT comments_posts_fkey FOREIGN KEY (pid)
+        REFERENCES public.posts (pid) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION,
+    CONSTRAINT comments_users_fkey FOREIGN KEY (uid)
+        REFERENCES public.users (uid) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+)
+WITH (
+    OIDS = FALSE
+)
+TABLESPACE pg_default;
+
 ALTER TABLE public.comments
+    OWNER to postgres;
+
+-- Table: public.favorites
+
+-- DROP TABLE public.favorites;
+
+CREATE TABLE public.favorites
+(
+    uid integer NOT NULL,
+    pid integer NOT NULL,
+    CONSTRAINT favorites_posts_fkey FOREIGN KEY (pid)
+        REFERENCES public.posts (pid) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION,
+    CONSTRAINT favorites_users_fkey FOREIGN KEY (uid)
+        REFERENCES public.users (uid) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+)
+WITH (
+    OIDS = FALSE
+)
+TABLESPACE pg_default;
+
+ALTER TABLE public.favorites
+    OWNER to postgres;
+
+-- Table: public.vector
+
+-- DROP TABLE public.vector;
+
+CREATE TABLE public.vector
+(
+    uid integer NOT NULL,
+    vector json NOT NULL,
+    CONSTRAINT vector_pkey PRIMARY KEY (uid),
+    CONSTRAINT vector_fkey FOREIGN KEY (uid)
+        REFERENCES public.users (uid) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+)
+WITH (
+    OIDS = FALSE
+)
+TABLESPACE pg_default;
+
+ALTER TABLE public.vector
     OWNER to postgres;
